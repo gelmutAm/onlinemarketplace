7f7e2b061ef0057c9510849e04a88914bda60559
Anastasiia Gelmut <anastasiia_gelmut@epam.com>
2020-08-19 14:25:24 +0400

    signup-modal confirm-password input added

--- signup-modal/signup-modal.js
+++ signup-modal/signup-modal.js
@@ -2,32 +2,37 @@ const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"
 const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/
 
 $('.signup-form').submit(e => {
-    debugger
     e.preventDefault();
 
     const emailMessage = 'Email is not valid!';
     const passwordMessage = 'Password must be at list 8 characters including a number, uppercase and lowercase letters.';
+    const confirmPasswordMessage = 'Please make sure your passwords match.';
 
     const emailInput = e.target[0];
     const passwordInput = e.target[1];
+    const confirmPasswordInput = e.target[2];
 
     const isEmailValid = EMAIL_REGEX.test(emailInput.value.toLowerCase());
-    const isPasswordValid = PASSWORD_REGEX.test(passwordInput.value)
+    const isPasswordValid = PASSWORD_REGEX.test(passwordInput.value);
+    const doPasswordsMatch = (passwordInput.value === confirmPasswordInput.value);
 
-    if (isEmailValid && isPasswordValid) {
+    if (isEmailValid && isPasswordValid && doPasswordsMatch) {
         hideModal('.signup-modal-wrapper');
-        emailInput.value = ''
-        passwordInput.value = ''
+        emailInput.value = '';
+        passwordInput.value = '';
+        confirmPasswordInput = '';
     } else {
         switch (true) {
             case !isEmailValid : emailInput.setCustomValidity(emailMessage); break;
             case !isPasswordValid : passwordInput.setCustomValidity(passwordMessage); break;
+            case !doPasswordsMatch : confirmPasswordInput.setCustomValidity(confirmPasswordMessage); break;
         }
-        e.target.reportValidity() 
+        e.target.reportValidity(); 
     }    
 })
 
 function resetValidity() {
     $('#signup-login')[0].setCustomValidity('');
     $('#signup-password')[0].setCustomValidity('');
+    $('#signup-confirm-password')[0].setCustomValidity('');
 }
